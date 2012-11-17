103b0379d4b0817fc29113b469b87448d5a314b9
Igor Escobar <blog@igorescobar.com>
2012-11-17 18:59:16 -0200

    a little experiment with twitter streaming API and nodejs

---
+++ twitter-streaming/twitter.js
@@ -0,0 +1,19 @@
+var twitter = require('ntwitter');
+var credentials = require('./credentials');
+
+var t = new twitter({
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
+});
+
+t.stream(
+    'statuses/filter',
+    { track: ['#ruby', '#php', '#dev', '#python', '#javascript'] },
+    function(stream) {
+        stream.on('data', function(tweet) {
+            console.log(tweet.text);
+        });
+    }
+);
