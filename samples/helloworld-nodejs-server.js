847fed031073b0501d438c091cd26092791f1199
Igor Escobar <blog@igorescobar.com>
2012-11-17 18:40:06 -0200

    movendo alguns arquivos para pasta sample

---
+++ samples/helloworld-nodejs-server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+// Server URL: http://localhost:8888/
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
