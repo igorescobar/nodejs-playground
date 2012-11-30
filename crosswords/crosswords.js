d2b9346eea0fb5f856afde21cdd31e6ac9134d55
Igor Escobar <igor.escobar@abril.com.br>
2012-11-30 18:47:34 -0200

    algorithm almost done.

--- crosswords/crosswords.js
+++ crosswords/crosswords.js
@@ -1,6 +1,6 @@
 var stdin   = process.stdin, 
     stdout  = process.stdout,
-    words   = new Array(),
+    words   = [],
     crossWordSignal = '*',
     maxWordLength   = 10,
     maxWordSlots    = maxWordLength * 10,
@@ -13,8 +13,8 @@ stdin.setEncoding('utf8');
 stdout.write('So.. do you want to play crosswords?\n\n\
 Start typing the words that you want to include on it: ');
 
-
-crossWord = function () {
+// empty crossword board
+var crossWord = function () {
   var crossWordLine = function () {
     for (var i = 1, lineDigit = crossWordSignal; i < maxWordLength; i++){
       lineDigit += crossWordSignal;
@@ -23,9 +23,7 @@ crossWord = function () {
     return lineDigit;
   };
 
-  var crossWord = Array(),
-      line = crossWordLine();
-  
+  var crossWord = [], line = crossWordLine();
   for(var i = 0; i < maxWordLength; i++){
     crossWord.push(line);
   }
@@ -64,56 +62,93 @@ var registerWord = function (word) {
   return words.push(word);
 };
 
-stdin.on('data', function (word) {
+var registerWordOnBoard = function (){
+  var currentWord,
+      coords = [],
+      direction = 'v';
   
+  for (i in words) {
+    currentWord = words[i];
+    
+    for (var col = 0; col < crossWord.length; col++) {
+      boardLine = direction === 'v' ? getColumn(col) : crossWord[col];
+      
+      var directions = {word: currentWord, 'direction': direction, 'position': col, 'coord': searchFreeSlot(boardLine, currentWord, 0)};
+      
+      if(directions.coord[0] !== directions.coord[1] && (directions.coord[1] - directions.coord[0]) === currentWord.length) {
+        direction = (direction === 'v') ? 'h' : 'v';
+        coords.push(directions);
+        processDirections(directions);
+        break;
+      }
+    }
+  }
+};
+
+var showBoard = function() {
+  console.log(crossWord.join('\n'));
+};
+
+stdin.on('data', function (word) {
   registerWord(word);
 
-  // automaticly exit the program if there's 
-  // no open slots left on the game.
   if(remainingSlots <= 0)
     process.exit(1);
 
 });
 
-// 0 = horizontal, 1 = vertical
-var verticalDirection = function () {
-  Math.floor((Math.random()*2)) == true
-};
-
-var searchHorizontalFreeSlot = function (line, word, start) {
+var searchFreeSlot = function (line, word, start) {
   var lineLength = line.length,
-      wordLength = word.length
+      wordLength = word.length;
 
   for(var col = start; col < lineLength; col++){
-    if(line[col] === crossWordSignal) break;
+    if(line[col] === crossWordSignal || line[col] === word[col]) break;
     start++
   }
 
   var end = start;
+
   for(var col = end; col < lineLength; col++){
     var rangeSize = end - start;
-    if (rangeSize === wordLength) break;
-    if (line[col] === crossWordSignal) {
+    if (rangeSize == wordLength) break;
+    if (line[col] === crossWordSignal || line[col] === word[col]) {
       end++
     } else {
-      return searchHorizontalFreeSlot(line, word, start + 1);
+      return searchFreeSlot(line, word, start + 1);
     }
   }
-  return [start,end];
+  
+  return [start, end];
+};
+
+var getColumn = function (col) {
+  var line = [];
+  for(var l = 0; l < crossWord.length; l++)
+    line.push(crossWord[l][col]);
+  
+  return line.join('');
+};
+
+var processDirections = function (directions) {
+  var setCharAt = function (str, index, chr) {
+    if(index > str.length - 1) return str;
+    return str.substr(0, index) + chr + str.substr(index + 1);
+  };
+
+  for(var i = directions.coord[0], w = 0; i < directions.coord[1]; i++) {
+    if (directions.direction === 'h') {
+      crossWord[directions.position] = setCharAt(crossWord[directions.position], i, directions.word[w]);
+    } else {
+      crossWord[i] = setCharAt(crossWord[i], directions.position, directions.word[w]);
+    }
+    w++;
+  }
 };
 
 process.on('exit', function () {
-  var currentWord;
-   
-   console.log(searchHorizontalFreeSlot('AAAAA***AA', 'TEST', 0));
-
-  // for(i in words){
-  //   currenWord = words[i];
-
-  //   for(var line = 0; line < crossWord.length; line++){
-  //     console.log(searchHorizontalFreeSlot(crossWord[line], currenWord, 0));
-  //   }
-  // }
+  registerWordOnBoard();
+  showBoard();
+  console.log('Have fun! :)');  
 }); 
 
 process.on('SIGINT', function () {
