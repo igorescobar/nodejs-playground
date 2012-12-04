cdca29e7f7fac3234a036acec2c2361cc660641b
Igor Escobar <blog@igorescobar.com>
2012-12-04 10:50:15 -0200

    using the readline module for STDIN/STDOUT

--- crosswords/start.js
+++ crosswords/start.js
@@ -5,21 +5,23 @@
     -   node start.js --words word1,word2,word3,word4
 */
 
-var CrossWords = require('./crosswords'),
+var readline = require('readline'),
+    CrossWords = require('./crosswords'),
     game = new CrossWords();
 
 if(game.wordsShortCut === false) {
-    game.stdout.write('Type the words that you want to ' + 
-                      'include on crossword game separed by comma: ');
     
-    game.stdin.resume();
-    game.stdin.setEncoding('utf8');
+    var rl = readline.createInterface(process.stdin, process.stdout);
 
-    game.stdin.on('data', function (words) {
+    rl.question("Type the words that you want to " + 
+                "include on crossword game separed by comma: ", function(words) {
+     
         game.processTypedWords(words);
         process.exit(1);
-    }); 
 
+        rl.close();
+    });
+    
 } else {
     game.processTypedWords(process.argv[3]);
 }
