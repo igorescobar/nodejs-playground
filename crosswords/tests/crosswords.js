5ff4b095e364ac44c6cc86bd6fc655960e436335
Igor Escobar <blog@igorescobar.com>
2012-12-04 22:56:09 -0200

    adding code coverage to the output of unit tests and get 100% of code coverage.

--- crosswords/tests/crosswords.js
+++ crosswords/tests/crosswords.js
@@ -1,5 +1,7 @@
-var assert = require('assert'),
-    CrossWords = require('../crosswords.js'),
+var jsc = require('jscoverage'),
+    assert = require('assert'),
+    require = jsc.require(module),
+    CrossWords = require('../crosswords.js', true),
     game = new CrossWords();
 
 module.exports = {
@@ -13,20 +15,24 @@ module.exports = {
     assert.equal(false, game.wordsShortCut);
     assert.equal(10, game.crossWord.length);
   },
-  'methods.drawCrossWord()': function(){
+  'methods.randomArray()': function () {
+    assert.notEqual(['word1', 'word2', 'word3', 'word4'], 
+                    game.randomArray(['word1', 'word2', 'word3', 'word4']));
+  },
+  'methods.drawCrossWord()': function () {
     var drawedCrossWord =  game.drawCrossWord();
     assert.equal(10, drawedCrossWord.length);
     drawedCrossWord.forEach(function(drawed){
       assert.equal('**********', drawed);
     }); 
   },
-  'methods.prepareWord()': function(){
+  'methods.prepareWord()': function () {
     assert.equal('ABC', game.prepareWord('abc'));
     assert.equal('ABC', game.prepareWord('a-b-c'));
     assert.equal('ABC', game.prepareWord('a   b    c'));
     assert.equal('BC', game.prepareWord('ábác'));
   },
-  'methods.validateWord()': function(){
+  'methods.validateWord()': function () {
     assert.equal("Your word 'palavramaiorque10chars' can't be higher than 10 chars.\n",
     game.validateWord('palavramaiorque10chars').error);
     game.remainingSlots = 0;
@@ -35,16 +41,22 @@ module.exports = {
     game.validateWord('palavra').error);
     game = new CrossWords(); // reseting classes
   },
-  'methods.processTypedWords()': function(){
+  'methods.processTypedWords()': function () {
     game.processTypedWords('yahoo,meme,poker');
 
     assert.equal(3, game.words.length); 
     assert.equal(86, game.remainingSlots);
   },
-  'methods.orderByLength()': function(){
+  'methods.orderByLength()': function () {
     assert.equal('yahoo', game.orderByLength(['a', 'bc', 'yahoo'])[0]); 
   },
-  'methods.registerWordsOnBoard().smallWords': function(){
+  'methods.registerWord()': function () {
+    game = new CrossWords();
+
+    assert.deepEqual(['SMALL'], game.registerWord('small'));
+    assert.deepEqual("Your word 'SUPERBIGWORD' can't be higher than 10 chars.\n", game.registerWord('superbigword'));
+  },
+  'methods.registerWordsOnBoard().smallWords': function () {
     game = new CrossWords();
     game.randomPositions = false;
 
@@ -64,14 +76,19 @@ module.exports = {
        "**********",
        "**********"], game.crossWord);
   },
-  'methods.registerWordsOnBoard().biggerWords': function(){
+  'methods.registerWordsOnBoard().biggerWords': function () {
     game = new CrossWords();
     game.randomPositions = false;
 
     game.processTypedWords('felicidade,geladeiras,pernambuco,camilinhas,respondera,comunidade,institutos,curriculos,dicionario');
     game.registerWordsOnBoard();
 
-    assert.deepEqual(["DICIONARIO","CURRICULOS","INSTITUTOS","COMUNIDADE","RESPONDERA","CAMILINHAS","PERNAMBUCO","GELADEIRAS","FELICIDADE"], game.words);
+    assert.deepEqual(["DICIONARIO","CURRICULOS",
+                      "INSTITUTOS","COMUNIDADE",
+                      "RESPONDERA","CAMILINHAS",
+                      "PERNAMBUCO","GELADEIRAS",
+                      "FELICIDADE"], game.words);
+
     assert.deepEqual(
       ["DICIONARIO",
        "CURRICULOS",
@@ -84,10 +101,32 @@ module.exports = {
        "FELICIDADE",
        "**********"], game.crossWord);
   },
-  'methods.showBoard()': function(){
+  'methods.showBoard().noRandomPositionsPlusNoobieMode': function () {
+    game = new CrossWords();
+    game.prettyPrint = false;
+    game.randomPositions = false;
+    game.processTypedWords('yahoo,meme,poker');
+    game.registerWordsOnBoard();
+    assert.deepEqual(
+      'P O K E R Y A H O O \n' + 
+      'M E M E * * * * * * \n' + 
+      '* * * * * * * * * * \n' + 
+      '* * * * * * * * * * \n' + 
+      '* * * * * * * * * * \n' + 
+      '* * * * * * * * * * \n' + 
+      '* * * * * * * * * * \n' + 
+      '* * * * * * * * * * \n' + 
+      '* * * * * * * * * * \n' + 
+      '* * * * * * * * * * ', game.showBoard());
+  },
+  'methods.showBoard()': function () {
     game = new CrossWords();
     game.processTypedWords('yahoo,meme,poker');
     game.registerWordsOnBoard();
     assert.deepEqual(209, game.showBoard().length);
   }
 };
+
+process.on('exit',function(){
+   jsc.coverage();
+});
