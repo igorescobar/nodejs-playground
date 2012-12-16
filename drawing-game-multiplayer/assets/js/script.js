72e0ad44b7b21c1d504688a3d36c026e9a88dbe9
Igor Escobar <blog@igorescobar.com>
2012-12-16 17:50:36 -0200

    more optimizations.

--- drawing-game-multiplayer/assets/js/script.js
+++ drawing-game-multiplayer/assets/js/script.js
@@ -1,10 +1,5 @@
 $(function(){
 
-	if(!('getContext' in document.createElement('canvas'))){
-		alert('Sorry, it looks like your browser does not support canvas!');
-		return false;
-	}
-
 	var url = 'http://localhost:8080'
 		,	doc = $(document)
 		,	win = $(window)
@@ -17,27 +12,37 @@ $(function(){
 		,	cursors = []
 		,	socket = io.connect(url)
 		,	prev = {};
-	
-	function handleUsers(data){
-		if(data.register_user){
-			
-			data = data.register_user;
-			if(!id) id = data.id
-			cursors[data.id] = $('<div class="cursor">').appendTo('#cursors').hide();
-			clients[data.id] = data;
-			
-		} else if(data.unregister_user){
 
-			data = data.unregister_user;
-			cursors[data.id].remove();
-			delete clients[data.id];
-			delete cursors[data.id];
+	var utils = {
+		handleUsers: function (data) {
+			if(data.register_user){
+				
+				data = data.register_user;
+				if(!id) id = data.id
+				cursors[data.id] = $('<div class="cursor">').appendTo('#cursors').hide();
+				clients[data.id] = data;
+				
+			} else if(data.unregister_user){
 
+				data = data.unregister_user;
+				cursors[data.id].remove();
+				delete clients[data.id];
+				delete cursors[data.id];
+
+			}
+		},
+		drawLine: function (fromx, fromy, tox, toy) {
+			ctx.moveTo(fromx, fromy);
+			ctx.lineWidth = 5;
+			ctx.lineTo(tox, toy);
+			ctx.strokeStyle = '#333';
+			ctx.lineCap = 'round';
+	  	ctx.stroke();
 		}
 	};
 
 	socket.on('message', function(data){	
-		handleUsers(JSON.parse(data));
+		utils.handleUsers(JSON.parse(data));
 	});
 
 	socket.on('moving', function (data) {
@@ -45,10 +50,10 @@ $(function(){
 			cursors[data.id].show().css({ 'left': data.x, 'top': data.y });
 		
 			if(data.drawing) {		
-				drawLine(data.prev_x, data.prev_y, data.x, data.y);
+				utils.drawLine(data.prev_x, data.prev_y, data.x, data.y);
 			}
 		}	else {
-			handleUsers({"register_user": {"id": data.id}});
+			utils.handleUsers({"register_user": {"id": data.id}});
 		}
 	});
 
@@ -71,21 +76,14 @@ $(function(){
 		});
 
 		if(drawing){
-			drawLine(prev.x, prev.y, e.pageX, e.pageY);
+			utils.drawLine(prev.x, prev.y, e.pageX, e.pageY);
 			prev.x = e.pageX;
 			prev.y = e.pageY;
 		}
-
 	});
 	
 	doc.on('mouseup mouseleave',function(){
 		drawing = false;
 	});
-
-	function drawLine(fromx, fromy, tox, toy){
-		ctx.moveTo(fromx, fromy);
-		ctx.lineTo(tox, toy);
-		ctx.stroke();
-	};
 
 });
